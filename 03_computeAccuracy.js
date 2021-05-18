be62ec9e57a4a8f5889ed7d487c365a93a11dcb2
Dhemerson Conciani <dhemerson.costa@ipam.org.br>
2021-05-18 12:07:53 -0300

    Update 03_computeAccuracy.js

--- 03_computeAccuracy.js
+++ 03_computeAccuracy.js
@@ -1,9 +1,10 @@
 // accuracy analysis for Guianas - collection 3
 // write to: dhemerson.costa@ipam.org.br and joaquim.pereira@ipam.org.br
 
-// Classes avaliable to Guianas (LAPIG validation points):
+// list with all classes avaliable to Guianas:
 // Sin consolidar
 // No Observado
+// Não Observado
 // Río, Lago u Océano
 // Formación Forestal
 // Mosaico de Agricultura y/o Pasto
@@ -28,18 +29,25 @@ var label = 'validation_c3_v' + version;
 // load LAPIG validation points
 var assetPoints = ee.FeatureCollection('users/vieiramesquita/MAPBIOMAS/mapbiomas_amazonia_50K_RAISG_plus_Brasil_v6');
 
+// load classification regions
+var raster_regions = ee.Image('projects/mapbiomas-raisg/DATOS_AUXILIARES/RASTERS/clasificacion-regiones-3');
+
+// extract region ID and paste into points
+var assetPoints = raster_regions.reduceRegions({
+  collection: assetPoints,
+  reducer: ee.Reducer.mean(),
+  scale: 30,
+});
+
 // load collection 3 classification (pre-integration)
 var image = ee.ImageCollection(root)
             .filterMetadata('version', 'equals', version)
             .min();
             
-// load classification regions (vector)
-var regions_vec = ee.FeatureCollection([]);
-
 // define regions to be processed
-var regioes = ['50201', '50202', '50203', '50204', '50205', '50903', '50904',
-               '60208', '60209',
-               '80206', '80207'
+var regioes = [50201, 50202, 50203, 50204, 50205, 50903, 50904,
+               60208, 60209,
+               80206, 80207
                ];
 
 // define years to be processed 
@@ -54,6 +62,7 @@ var anos = [
 var excludedClasses = [
     "Sin consolidar",
     "No Observado",
+    "Não Observado",
     "Manglar",
     "Error"
 ];
@@ -68,6 +77,9 @@ var classes = {
   "Río, Lago u Océano": 33,
 };
 
+// create class dictionary 
+classes = ee.Dictionary(classes);
+
 // define mapbiomas color ramp
 var palettes = require('users/mapbiomas/modules:Palettes.js');
 var vis = {
@@ -76,7 +88,62 @@ var vis = {
     'palette': palettes.get('classification2')
 };
 
-// plot map
-Map.addLayer(image.select(['classification_2020']), vis, 'classification');
-Map.addLayer(assetPoints, {}, 'points');
+// create empty recipe
+var recipe_regiao = ee.List([]);
+var recipe_ano = ee.List([]);
+var recipe_acc = ee.List([]);
 
+//// Get samples for each year and export using for-loop///
+for (var Year in anos){
+  var year = anos[Year];
+  var ano = anos[Year];
+  
+  var class_GAP = image.select('classification_' + ano);
+  var class_GAP = class_GAP.updateMask(class_GAP.neq(0));
+
+  var amostraTotal = ee.FeatureCollection(assetPoints)
+      .filterMetadata('POINTEDITE', 'not_equals', 'TRUE')
+      .filter(ee.Filter.inList('CLASS_' + ano, excludedClasses).not())
+    .map(
+        function (feature) {
+            return feature.set('year', ano)
+                .set('reference', classes.get(feature.get('CLASS_' + ano)));
+        }
+    );
+    
+    for (var i_reg=0;i_reg<regioes.length; i_reg++){
+      var regiao = regioes[i_reg];
+      var limite = raster_regions.updateMask(raster_regions.eq(regiao));
+      
+      var class_GAP_reg = class_GAP.updateMask(limite);
+      
+      var pt_reg = amostraTotal.filterMetadata('mean', 'equals', regiao);
+
+      var valida_reg1 = class_GAP_reg.sampleRegions({collection: pt_reg, properties: ['reference'], scale: 30, geometries: false});
+      
+      // calc accuracy
+      var accuracy= valida_reg1.errorMatrix('classification_' + ano,'reference').accuracy();
+      
+      // update lists
+      recipe_regiao = recipe_regiao.add(regiao);
+      recipe_ano = recipe_ano.add(ano);
+      recipe_acc = recipe_acc.add(accuracy);
+      }
+}
+
+// create array with results
+var result_array = ee.Array.cat([recipe_regiao, recipe_ano, recipe_acc], 1);
+print ('result array', result_array);
+
+// convert array into feature
+var feature_array = ee.Feature(null, {
+  result_array:result_array
+});
+
+// export CSV to googleDrive
+Export.table.toDrive({
+  collection: ee.FeatureCollection(feature_array),
+  description: label,
+  folder: 'VALIDATION_CSV_C3',
+  fileFormat: 'CSV'
+});
